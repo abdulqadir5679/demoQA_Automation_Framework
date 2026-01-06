4bb4bdf1b79acb9d8e97852fa9d4a9fb8d215082
Abdul Qadir <abdul.m.qadir@contour-software.com>
2026-01-06 17:36:59 +0500

    progress bar

---
+++ tests/widgets_demoQA/progress-bar.spec.js
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('Test suite', () => {
+    test('Test case', async ({page}) => {
+        await page.goto('https://demoqa.com/progress-bar');
+
+        await page.locator('#startStopButton').click();
+
+        await page.waitForTimeout(5000);
+
+        await page.locator('#startStopButton').click();
+
+        // Assert progress bar value is around 9%
+        await expect(page.locator('.progress-bar')).toHaveAttribute('aria-valuenow', '50');
+    });
+});
