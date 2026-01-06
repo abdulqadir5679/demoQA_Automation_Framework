8e2369fe39a8e533e6a23a2d873456675a2031ae
Abdul Qadir <abdul.m.qadir@contour-software.com>
2026-01-06 12:22:57 +0500

    Form application automated

--- tests/projects/form-application.spec.js
+++ tests/projects/form-application.spec.js
@@ -7,9 +7,22 @@ test.describe('Elements Suite => Form', ()=> {
         await page.fill('#firstName', 'Abdul');
         await page.fill('#lastName', 'Qadir');
         await page.fill('#userEmail', 'aq@gmail.com');
-        // await page.locator('.custom-control-label').isChecked();
-        await page.locator('.custom-control.custom-radio.custom-control-inline').check();
+        await page.locator('label[for="gender-radio-1"]').check();
+        await page.fill('#userNumber', '1234567890');
+        
+        await page.fill('#currentAddress', 'Karachi, Pakistan');
 
+        // Select state
+        await page.locator('#state').click();
+        await page.locator('#react-select-3-option-0').click();
 
+        // Select city
+        await page.locator('#city').click();
+        await page.locator('#react-select-4-option-0').click();
+
+        await page.click('#submit');
+
+        // Assert success message
+        await expect(page.getByText('Thanks for submitting the form')).toBeVisible();
     })
 })
